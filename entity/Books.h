b9af7e73c0bba2ec496527bea7735d90c0fe6c14
handriss <andrashinkel@gmail.com>
2017-01-26 19:06:28 +0100

    basic class working

---
+++ entity/Books.h
@@ -0,0 +1,17 @@
+//
+// Created by handris on 1/25/17.
+//
+
+#ifndef INC_1STSI_BOOKS_H
+#define INC_1STSI_BOOKS_H
+
+
+struct Books {
+    char  title[50];
+    char  author[50];
+    char  subject[100];
+    int   book_id;
+};
+
+
+#endif //INC_1STSI_BOOKS_H
--- main.c
+++ main.c
@@ -1,27 +1,38 @@
 #include <stdio.h>
+#include <string.h>
+#include "entity/Books.h"
 
-int main()
-{
-    int a, b, c;
-    char ch;
+int main( ) {
 
-    while (1) {
-        printf("Inut two integers\n");
-        scanf("%d%d", &a, &b);
-        getchar();
+    int maci[5] = {1, 45, 34, 5, 6};
+    printf("%a", maci);
 
-        c = a + b;
+    struct Books Book1;        /* Declare Book1 of type Book */
+    struct Books Book2;        /* Declare Book2 of type Book */
 
-        printf("(%d) + (%d) = (%d)\n", a, b, c);
+    /* book 1 specification */
+    strcpy( Book1.title, "C Programming");
+    strcpy( Book1.author, "Nuha Ali");
+    strcpy( Book1.subject, "C Programming Tutorial");
+    Book1.book_id = 6495407;
 
-        printf("Do you wish to add more numbers (y/n)\n");
-        scanf("%c", &ch);
+    /* book 2 specification */
+    strcpy( Book2.title, "Telecom Billing");
+    strcpy( Book2.author, "Zara Ali");
+    strcpy( Book2.subject, "Telecom Billing Tutorial");
+    Book2.book_id = 6495700;
 
-        if (ch == 'y' || ch == 'Y')
-            continue;
-        else
-            break;
-    }
+    /* print Book1 info */
+    printf( "Book 1 title : %s\n", Book1.title);
+    printf( "Book 1 author : %s\n", Book1.author);
+    printf( "Book 1 subject : %s\n", Book1.subject);
+    printf( "Book 1 book_id : %d\n", Book1.book_id);
+
+    /* print Book2 info */
+    printf( "Book 2 title : %s\n", Book2.title);
+    printf( "Book 2 author : %s\n", Book2.author);
+    printf( "Book 2 subject : %s\n", Book2.subject);
+    printf( "Book 2 book_id : %d\n", Book2.book_id);
 
     return 0;
 }
